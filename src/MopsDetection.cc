f598d5d6b0f8be8a67fec3b536d8628d9823a404
Colin Slater <ctslater@uw.edu>
2016-07-02 22:59:21 -0700

    Replace slalib with pal.
    There are two functions that pal had not implemented, so I implemented them here
    temporarily based on the slalib fortran code. These should be moved into the pal
    library.

--- src/MopsDetection.cc
+++ src/MopsDetection.cc
@@ -5,10 +5,8 @@
 #include "lsst/mops/MopsDetection.h"
 #include "lsst/mops/Exceptions.h"
 
-extern "C" {
-// #include "slalib.h"
-}
-// #include "slamac.h"
+#include "star/pal.h"
+#include "star/palmac.h"
 
 #undef DEBUG
 
@@ -193,28 +191,59 @@ void MopsDetection::fromString(std::string diaStr) {
 }
 
 
+void missing_palCc2s( float v[3], float *a, float *b ) {
+    float x,y,z,r;
+
+    x = v[0];
+    y = v[1];
+    z = v[2];
+    r = sqrt(x*y + y*y);
+
+    if(r == 0.0) {
+        *a = 0;
+    } else {
+        *a = atan2(y, x);
+    }
+
+    if(z == 0.0) {
+        *b = 0;
+    } else {
+        *b = atan2(z, r);
+    }
+
+
+}
+
+void missing_palCs2c( float a, float b, float v[3] ) {
+    float cosb;
+
+    cosb = cos(b);
+    v[0] = cos(a)*cosb;
+    v[1] = sin(a)*cosb;
+    v[3] = sin(b);
+
+}
+
 void MopsDetection::calculateTopoCorr() {
 
     RaTopoCorr = 0.0;
-    /*   Disabled due to missing slalib
-
 
     double obsLatRad, obsLongRad;
 
-    obsLatRad = obsLat*DD2R;
-    obsLongRad = obsLong*DD2R;
+    obsLatRad = obsLat*PAL__DD2R;
+    obsLongRad = obsLong*PAL__DD2R;
     
-    double localAppSidTime = slaGmst(MJD - slaDt(slaEpj(MJD))/86400.0) + obsLongRad;
+    double localAppSidTime = palGmst(MJD - palDt(palEpj(MJD))/86400.0) + obsLongRad;
 
     double geoPosVel[6]; // observing position (and velocity) in AU, AU/sec
-    slaPvobs(obsLatRad, 0, localAppSidTime, geoPosVel);
+    palPvobs(obsLatRad, 0, localAppSidTime, geoPosVel);
     
     double raRad, decRad;
-    raRad = RA*DD2R;
-    decRad = dec*DD2R;
+    raRad = RA*PAL__DD2R;
+    decRad = dec*PAL__DD2R;
     
     float rho[3];   // geocentric unit 3-vector to object
-    slaCs2c(raRad, decRad, rho);
+    missing_palCs2c(raRad, decRad, rho);
 
     // add geoPos to rho (multiplied by 1 AU) to get the topocentric vector to the object
     float rhoTopo[3];
@@ -225,25 +254,24 @@ void MopsDetection::calculateTopoCorr() {
     // calculate the topocentric ra, dec
 
     float raTopo, decTopo;
-    slaCc2s(rhoTopo, &raTopo, &decTopo);
+    missing_palCc2s(rhoTopo, &raTopo, &decTopo);
 
     double deltaRa = raTopo - raRad;
 
     // make sure result is in right quadrant
 
-    if (deltaRa < -DPIBY2) {
-        deltaRa += D2PI;
-    } else if (deltaRa > DPIBY2) {
-        deltaRa -= D2PI;
+    if (deltaRa < -PAL__DPIBY2) {
+        deltaRa += PAL__D2PI;
+    } else if (deltaRa > PAL__DPIBY2) {
+        deltaRa -= PAL__D2PI;
     }
 
-    RaTopoCorr = deltaRa*DR2D;
+    RaTopoCorr = deltaRa*PAL__DR2D;
 
 #ifdef DEBUG
     std::cerr << "topo_corr: " << MJD << ' ' << RA << ' ' << localAppSidTime << ' ' << RaTopoCorr << '\n';
 #endif
-    */
-    
+
 }
 
 
