b5f64609c4818acf07ebbc63023fb1e8aa667ad5
Colin Slater <ctslater@umich.edu>
2016-11-21 18:49:49 -0600

    Cleanups for makeTracklets.py

--- python/lsst/mops/daymops/makeTracklets.py
+++ python/lsst/mops/daymops/makeTracklets.py
@@ -1,22 +1,16 @@
 
 from __future__ import print_function, division
 
-# import lsst.mops.daymops as mops
 import sys
-import lsst.afw.geom as afwGeom
 import lsst.pipe.base as pipeBase
 import lsst.pex.config as pexConfig
 from lsst.pipe.base import ArgumentParser, TaskError
-from lsst.daf.base import DateTime
 import traceback
-import itertools
 import sqlite3
 
 import numpy as np
 
 from . import findTracklets, findTrackletsConfig, MopsDetection, TrackletSet, doCollapsingPopulateOutputVector
-
-from astropy.io import ascii
 
 __all__ = ("MakeTrackletsTask", "MakeTrackletsConfig")
 
@@ -45,11 +39,9 @@ class MakeTrackletsConfig(pexConfig.Config):
 
 class MakeTrackletsTaskRunner(pipeBase.TaskRunner):
 
-
     @staticmethod
     def getTargetList(parsedCmd, **kwargs):
         return [parsedCmd.id.refList]
-
 
     def __call__(self, dataRefList):
         task = self.makeTask()
@@ -65,6 +57,8 @@ class MakeTrackletsTaskRunner(pipeBase.TaskRunner):
 
                 if not isinstance(e, TaskError):
                     traceback.print_exc(file=sys.stderr)
+        return result
+
 
 class MakeTrackletsTask(pipeBase.CmdLineTask):
     ConfigClass = MakeTrackletsConfig
@@ -99,31 +93,8 @@ class MakeTrackletsTask(pipeBase.CmdLineTask):
 
         return detections
 
-    def write_out_tracklets(self, detections, tracklets, output_filename):
-
-        f_out = open(output_filename, "w")
-        print("# Visit, MJD, RA, Dec, SNR", file=f_out)
-
-        fmt_str = "{:d},{:f},{:f},{:f},{:.1f}"
-        for tracklet in tracklets:
-            indices = list(tracklet.indices())
-            detection_params = []
-            for det_index in indices:
-                visit_str = fmt_str.format(detections[det_index].ImageID,
-                                           detections[det_index].EpochMJD,
-                                           detections[det_index].RA,
-                                           detections[det_index].Dec,
-                                           detections[det_index].SNR)
-                detection_params.append(visit_str)
-            output_string = ",".join(detection_params)
-            print(output_string, file=f_out)
-        f_out.close()
-
-
     @pipeBase.timeMethod
     def run(self, dataRefList):
-
-        first_visit_id = min([ref.dataId['visit'] for ref in dataRefList])
 
         detections = []
         for ref in dataRefList:
@@ -142,11 +113,8 @@ class MakeTrackletsTask(pipeBase.CmdLineTask):
         for n, det in enumerate(detections):
             det.index = n
 
-        # write_out_detections_ds9(detections)
-
         tracklets = findTracklets(detections, self.config.getFindTrackletsConfig())
         print("Number of uncollapsed tracklets: {:d}".format(len(tracklets)))
-
 
         # Need to convert these to config settings
         # Tolerances in RA, Dec, Angle, Velocity
@@ -165,35 +133,26 @@ class MakeTrackletsTask(pipeBase.CmdLineTask):
                                          maxRMS, verbose)
         print("Number of collapsed tracklets: {:d}".format(len(collapsed_tracklets)))
         self.write_out_sqlite(detections, collapsed_tracklets)
-        sys.exit(0)
 
-        sel_three_det, = np.where([len(t.indices()) > 2 for t in collapsed_tracklets])
-        print("Number of 3-detection tracklets: {:d}".format(len(sel_three_det)))
-
-        sel_four_det, = np.where([len(t.indices()) > 3 for t in collapsed_tracklets])
-        print("Number of 4-detection tracklets: {:d}".format(len(sel_four_det)))
-        self.write_out_tracklets(detections,
-                                 [collapsed_tracklets[n] for n in sel_four_det],
-                                 "fourdet_tracklets_{:d}".format(first_visit_id))
-
-        # write_out_tracklets_ds9(collapsed_tracklets, detections)
-
-        return pipeBase.Struct(tracklets=collapsed_tracklets)
+        return pipeBase.Struct(tracklets=collapsed_tracklets,
+                               detections=detections)
 
     def write_out_sqlite(self, detections, tracklets):
-        db_conn = sqlite3.connect("tracklets.db")
+        db_conn = sqlite3.connect("tracklets.db", timeout=60)
         cursor = db_conn.cursor()
+        cursor.execute("PRAGMA journal_mode=WAL")
 
         cursor.execute("CREATE TABLE IF NOT EXISTS tracklets "
                        "(id INTEGER PRIMARY KEY, "
                        "MJD REAL, center_ra REAL, center_dec REAL)")
         cursor.execute("CREATE TABLE IF NOT EXISTS detections "
-                       "(id INTEGER PRIMARY KEY, visit VISIT, MJD REAL, ra REAL, dec REAL)")
+                       "(id INTEGER PRIMARY KEY, visit VISIT, MJD REAL, ra REAL, dec REAL, snr REAL)")
         cursor.execute("CREATE TABLE IF NOT EXISTS tracklet_links "
                        "(id INTEGER PRIMARY KEY, "
                        "tracklet INT, detection INT, "
                        "FOREIGN KEY(tracklet) REFERENCES tracklets(id), "
                        "FOREIGN KEY(detection) REFERENCES detections(id))")
+        db_conn.commit()
 
         detections_to_load = set()
         for tracklet in tracklets:
@@ -201,9 +160,9 @@ class MakeTrackletsTask(pipeBase.CmdLineTask):
 
         for det_id in detections_to_load:
             detection = detections[det_id]
-            cursor.execute("INSERT INTO detections VALUES (?,?,?,?,?)",
+            cursor.execute("INSERT INTO detections VALUES (?,?,?,?,?,?)",
                            (detection.ID, detection.ImageID, detection.EpochMJD,
-                            detection.RA, detection.Dec))
+                            detection.RA, detection.Dec, detection.SNR))
 
         for tracklet in tracklets:
             indices = tracklet.indices()
@@ -220,60 +179,14 @@ class MakeTrackletsTask(pipeBase.CmdLineTask):
                                "(NULL, ?, ?)",
                                (tracklet_id, detections[idx].ID))
 
-
-
-            # set([0L, 13391L, 3540L, 6855L])
         db_conn.commit()
 
-        #import pdb
-        #pdb.set_trace()
-
         db_conn.close()
-
-    def write_out_detections_ds9(detections):
-        """Debuging tool for writing out a ds9 region file.
-        """
-        det_region = open("detections.reg", "w")
-        print("ICRS", file=det_region)
-        colors = ['red', 'green', 'blue', 'yellow', 'pink', 'cyan']
-        color_iter = itertools.cycle(colors)
-        last_visit = 0
-        for n, det in enumerate(detections):
-            if det.ImageID != last_visit:
-                visit_color = color_iter.next()
-                last_visit = det.ImageID
-            print("point({:f},{:f}) # point=x color={:s} text=\"{:d}\"".format(det.RA, det.Dec,
-                                                                 visit_color, n),
-                  file=det_region)
-        det_region.close()
-
-    def write_out_tracklets_ds9(tracklets, detections):
-        tracklet_region = open("tracklets.reg", "w")
-        print("ICRS", file=tracklet_region)
-
-        colors = ['red', 'green', 'blue', 'yellow', 'pink', 'cyan']
-        color_iter = itertools.cycle(colors)
-
-        for tracklet in tracklets:
-            indices = list(tracklet.indices())
-            if len(indices) == 2:
-                continue
-
-            color = color_iter.next()
-            for index1, index2 in zip(indices[:-1], indices[1:]):
-                det1 = detections[index1]
-                det2 = detections[index2]
-                print("line({:f},{:f},{:f},{:f}) # color={:s}".format(det1.RA, det1.Dec,
-                                                                      det2.RA, det2.Dec,
-                                                                      color),
-                  file=tracklet_region)
-        tracklet_region.close()
-
 
     # Overriding these two functions prevent the task from attempting to save the config.
     # Can remove these when we have mapper entries for the config.
     def _getConfigName(self):
         return None
+
     def _getMetadataName(self):
         return None
-
